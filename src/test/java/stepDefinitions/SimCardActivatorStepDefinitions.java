ab74960bd5454c0f0111251213a4d57a737420b7
Josie Leye <jiley1@student.monash.edu>
2024-07-12 14:37:58 +1000

    Add test files

--- src/test/java/stepDefinitions/SimCardActivatorStepDefinitions.java
+++ src/test/java/stepDefinitions/SimCardActivatorStepDefinitions.java
@@ -1,7 +1,15 @@
 package stepDefinitions;
 
+import au.com.telstra.simcardactivator.Sim;
 import au.com.telstra.simcardactivator.SimCardActivator;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,5 +22,34 @@ import org.springframework.test.context.ContextConfiguration;
 public class SimCardActivatorStepDefinitions {
     @Autowired
     private TestRestTemplate restTemplate;
+
+    private Sim sim;
+
+    @Given("a functional sim card")
+    public void aFunctionalSimCard() {
+        sim = new Sim("1255789453849037777", "functional@sim.com", false);
+    }
+
+    @Given("a broken sim card")
+    public void aBrokenSimCard() {
+        sim = new Sim("8944500102198304826", "broken@sim.com", false);
+    }
+
+    @When("a request to activate the sim card is submitted")
+    public void aRequestToActivateTheSimIsSubmitted() {
+        this.restTemplate.postForObject("http://localhost:8080/activate", sim, String.class);
+    }
+
+    @Then("the sim card is activated and its state is recorded to the database")
+    public void theSimCardIsActivatedAndItsStateIsRecordedToTheDatabase() {
+        var simCard = this.restTemplate.getForObject("http://localhost:8080/query?simCardId={simCardId}", Sim.class, 1);
+        assertTrue(simCard.getActiveStatus());
+    }
+
+    @Then("the sim card fails to activate and its state is recorded to the database")
+    public void theSimCardFailsToActivateAndItsStateIsRecordedToTheDatabase() {
+        var simCard = this.restTemplate.getForObject("http://localhost:8080/query?simCardId={simCardId}", Sim.class, 2);
+        assertFalse(simCard.getActiveStatus());
+    }
 
 }
